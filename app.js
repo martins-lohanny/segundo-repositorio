42cf9c94801920d904e74c3524963e02e4e3f82c
martins-lohanny <132685337+martins-lohanny@users.noreply.github.com>
2024-11-05 21:40:03 -0300

    aula 05/11

---
+++ app.js
@@ -0,0 +1,26 @@
+function criaCartao (item, pergunta, resposta) {
+    //console.log(item, pergunta,resposta)
+
+    let flashcard = document.getElementById('flashcard');
+    let cartao = document.createElement('article');
+
+    cartao.className='cartao';
+
+    cartao.innerHTML=`
+        <div class="cartao_conteudo"
+                <h3>Missão</h3>
+
+                <div class="cartao__conteudo__pergunta">
+                   <p> Saiba qual e a missao de nossa empresa! </p>
+                </div>
+
+                <div class="cartao_conteudo_resposta">
+                    <p>Nossa missao e... </p>
+                </div>
+            </div>
+
+    `
+    flashcard.appendChield(cartao);
+    
+
+}
