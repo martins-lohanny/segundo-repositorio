bd51e58c887bbd56ea367f9f8b3ecb2c9473c7f1
martins-lohanny <132685337+martins-lohanny@users.noreply.github.com>
2024-11-12 21:00:41 -0300

    12/11

--- app.js
+++ app.js
@@ -1,33 +1,31 @@
-function criaCartao (item, pergunta, resposta) {
-    //console.log(item, pergunta,resposta)
+function criaCartao(item, pergunta, resposta) {
+    //console.log(item, pergunta, resposta)
+    let flashcard = document.getElementById('flashcard')
+    let cartao = document.createElement('article')
+    cartao.className = 'cartao'
 
-    let flashcard = document.getElementById('flashcard');
-    let cartao = document.createElement('article');
-
-    cartao.className='cartao';
-
-    cartao.innerHTML=`
-        <div class="cartao_conteudo"
-                <h3>Missão</h3>
-
-                <div class="cartao__conteudo__pergunta">
-                   <p> Saiba qual e a missao de nossa empresa! </p>
-                </div>
-
-                <div class="cartao_conteudo_resposta">
-                    <p>Nossa missao e... </p>
-                </div>
+    cartao.innerHTML = `
+        <div class="cartao__conteudo">
+            <h3>${item}</h3>
+            
+            <div class="cartao__conteudo__pergunta">
+                <p>${pergunta}</p>
             </div>
+            
+            <div class="cartao__conteudo__resposta">
+                <p>${resposta}</p>
+            </div>
+        </div>
     `
-    let respostaEstaVisivel - false;
-    function viraCartao (){
-        respostaEstaVisivel= !variavelEstaVisivel;
-        cartao.classList.toggle('active', viraCartao);
+    let respostaEstaVisivel = false
+
+    function viraCartao() {
+        respostaEstaVisivel = !respostaEstaVisivel
+        cartao.classList.toggle('active', respostaEstaVisivel)
     }
 
-    cartao.addEventListener('click', viraCartao);
+    cartao.addEventListener('click', viraCartao)
 
-    flashcard.appendChield(cartao);
-    
+    flashcard.appendChild(cartao)
 
 }
