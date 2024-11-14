c6c57be20c18b5038249c079302f638e39f4c500
martins-lohanny <132685337+martins-lohanny@users.noreply.github.com>
2024-11-13 22:40:45 -0300

    n

--- missaoVisaoValores.js
+++ missaoVisaoValores.js
@@ -1,19 +1,17 @@
 criaCartao (
     'Missão',
     'Saiba qual é a missão de nossa empresa!',
-    'Encantar nossos clientes com doces artesanais
-    de alta qualidade, criados com paixão e ingredientes selecionados, proporcionando momentos únicos de alegria e sabor.'
+    'Encantar nossos clientes com doces artesanais de alta qualidade,criados com paixão e ingridientes selecionados, proporcionando momentos unicos de alegria e sabor. '
+)
 
 criaCartao (
     'Visão',
     'Saiba qual é a visão de nossa empresa!',
-    'Ser referencia nas docerias, ser reconhecida por inovaçoes, ter compromisso com a qualidade e sustentabilidade. Mantendo sempre o amor pelo o que fazemos.'
+    'Ser referencia nas docerias,ser reconhecida por inovaçoes,ter compromisso com a qualidade e sustentabilidade.Mantendo sempre o amor pelo o que fazemos.'
 )
 
 criaCartao (
     'Valores',
     'Saiba quais são os valores de nossa empresa!',
-    'Qualidade e inovação
-Sustentabilidade
-Companherismo e Alegria.'
+    'Qualidade e inovação.Sustentabilidade.Companherismo e Alegria.'
 )
