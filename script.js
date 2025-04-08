3731537c505c32cc4ab5bd97a07e82adeaa4aae9
martins-lohanny <132685337+martins-lohanny@users.noreply.github.com>
2025-04-07 22:51:52 -0300

    07/04

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function(){
+    const aumentaFonteBotao = document.getElementById(‘aumentarfonte’);
+    aumentaFonteBotao.addEventListener( ‘click’ , function(){ 
+
+
+        })
+    })
